1bb43a132a911464c4a41a967e0bd872d3092892
sudarshaMug <sudarshamugiti@gmail.com>
2024-11-28 17:06:37 +0530

    modified changes

--- client/src/components/surveys/SurveyList.js
+++ client/src/components/surveys/SurveyList.js
@@ -9,8 +9,8 @@ class Surveylist extends Component {
   renderSurveys(){
     return this.props.surveys.reverse().map(survey=> {
         return(
-            <div className="card  darken-1 " key={survey._id}>
-                <div className="card-content ">
+            <div className="card darken-1" key={survey._id}>
+                <div className="card-content">
                     <span className="card-title">{survey.title}</span>
                     <p >{survey.body}</p>
                     <p className="right">Sent On: {new Date(survey.dateSent).toLocaleDateString()}</p>
