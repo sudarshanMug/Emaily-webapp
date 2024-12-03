a822d173054c17e48568835b2a0906c8c40af800
sudarshaMug <sudarshamugiti@gmail.com>
2024-12-03 18:11:04 +0530

    first commit

---
+++ client/src/components/surveys/SurveyNew.jsx
@@ -0,0 +1,48 @@
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { submitSurvey } from "../../store/slices/surveySlice";
+import { fetchUser } from "../../store/slices/authSlice";
+import SurveyForm from "./SurveyForm";
+import SurveyFormReview from "./SurveyFormReview";
+import { useNavigate } from "react-router-dom";
+
+const SurveyNew = () => {
+  const [showFormReview, setShowFormReview] = useState(false);
+  const [formValues, setFormValues] = useState({});
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const handleFormSubmit = (values) => {
+    setFormValues(values);
+    setShowFormReview(true);
+  };
+
+  const handleSurveySubmit = async () => {
+    console.log('Form Values', formValues)
+    await dispatch(submitSurvey({ values: formValues, navigate }));
+    dispatch(fetchUser());
+  };
+
+  const renderContent = () => {
+    if (showFormReview) {
+      return (
+        <SurveyFormReview
+          onCancel={() => setShowFormReview(false)}
+          formValues={formValues}
+          onSurveySubmit={handleSurveySubmit}
+        />
+      );
+    }
+
+    return (
+      <SurveyForm
+        onSurveySubmit={handleFormSubmit}
+        initialValues={formValues}
+      />
+    );
+  };
+
+  return <div>{renderContent()}</div>;
+};
+
+export default SurveyNew;
